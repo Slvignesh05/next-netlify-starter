149d66397f30e77657784ccb79124cb27f953af2
Slvignesh05 <169062599+Slvignesh05@users.noreply.github.com>
2025-02-10 21:07:32 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import { useEffect } from 'react'
 
 export default function Home() {
+  useEffect(() => {
+    let cookies = document.cookie;
+    if (!cookies) {
+      document.cookie = "test_cookie=HelloWorld; path=/";
+      alert("No cookie found. Setting a test cookie.");
+    } else {
+      alert("Cookies: " + cookies);
+    }
+  }, []);
+
   return (
     <div className="container">
       <Head>
@@ -15,7 +26,6 @@ export default function Home() {
         <p className="description" style={{ fontSize: '24px', fontWeight: 'bold', textAlign: 'center' }}>
           PoC by w3shi (S. Lakshmi Vignesh)(HackerOne)
         </p>
-
       </main>
 
       <Footer />
